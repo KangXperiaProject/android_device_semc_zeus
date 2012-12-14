035ead6a4d472dac76c440fa3fe4f703c5ba769c
Arnav Gupta <championswimmer@gmail.com>
2012-12-14 18:01:19 +0530

    zeus : device part for 2 stage boot
    Change-Id: If4048b12cf393850da722a5cf156e9e615e06303
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- zeus.mk
+++ zeus.mk
@@ -15,8 +15,7 @@ PRODUCT_COPY_FILES += \
     device/semc/zeus/prebuilt/init.semc.usb.rc:root/init.semc.usb.rc \
     device/semc/zeus/prebuilt/hw_config.sh:system/etc/hw_config.sh \
     device/semc/msm7x30-common/prebuilt/logo_H.rle:root/logo.rle \
-    device/semc/zeus/prebuilt/bootrec-device:root/sbin/bootrec-device \
-    device/semc/zeus/recovery.fstab:root/recovery.fstab
+    device/semc/zeus/recovery/bootrec-device:recovery/bootrec-device
 
 # Device specific configs
 PRODUCT_COPY_FILES += \
